f1811f9bd457a9eb5388fb72bf710552093acc3b
Ryan McCrystal <rrmccrystal@gmail.com>
2020-03-30 04:21:10 +0000

    Added support for www.covid2019.tech

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git pull --force
+docker build -t covid .
+docker stop covid
+docker rm covid
+docker run --name covid -e VIRTUAL_HOST=covid2019.tech,www.covid2019.tech -e VIRTUAL_PORT=8080 -e LETSENCRYPT_HOST=covid2019.tech -e LETSENCRYPT_EMAIL=rrmccrystal@gmail.com --network=draven_default --restart unless-stopped -d covid:latest
