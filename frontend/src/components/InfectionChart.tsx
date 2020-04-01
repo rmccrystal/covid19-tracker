b21fe1f93077b7b6630a83d4c060403fc61a4596
Ryan McCrystal <rrmccrystal@gmail.com>
2020-03-31 15:22:43 -1000

    Abbreviated numbers in chart

--- frontend/src/components/InfectionChart.tsx
+++ frontend/src/components/InfectionChart.tsx
@@ -1,7 +1,8 @@
 import React, {Component} from "react";
-import {Card} from "@blueprintjs/core";
+import {Card, Colors} from "@blueprintjs/core";
 import HistoricalInfectionEntry from "../shared/HistoricalInfectionEntry";
 import {ResponsiveLine, SliceTooltipProps} from "@nivo/line";
+import {Theme} from "@nivo/core";
 import InfectionEntrySelector from "./InfectionEntrySelector";
 
 import "./InfectionChart.scss"
@@ -9,6 +10,7 @@ import "./InfectionChart.scss"
 interface InfectionChartProps {
     entries: HistoricalInfectionEntry[]
     type: "infections" | "dead" | "recovered"
+    darkMode: boolean
 }
 
 interface InfectionChartState {
@@ -19,12 +21,8 @@ export default class InfectionChart extends Component<InfectionChartProps, Infec
     constructor(props: InfectionChartProps) {
         // Sort the entries by most cases to least
         super(props);
-        this.sortInfectionEntries()
+        this.sortInfectionEntries();
         this.state = {selectedEntry: props.entries[0]};
-    }
-
-    componentDidUpdate() {
-        this.sortInfectionEntries()
     }
 
     sortInfectionEntries() {
@@ -32,11 +30,13 @@ export default class InfectionChart extends Component<InfectionChartProps, Infec
     }
 
     render() {
+        this.sortInfectionEntries();
         let data = [{
             id: "Infections", data: this.state.selectedEntry.infections.map(value => {
                 return {x: value[0], y: value[1]}
             })
         }];
+
         return (
             <Card className="chart-card m-3">
                 <div className="text-center">
@@ -46,16 +46,18 @@ export default class InfectionChart extends Component<InfectionChartProps, Infec
                             this.setState({selectedEntry: entry as HistoricalInfectionEntry})
                         }}
                         defaultEntry={this.state.selectedEntry}/>
-                        <h2>Past {this.state.selectedEntry.getDaysSinceFirstCase()} days</h2>
+                    <h2>Past {this.state.selectedEntry.getDaysSinceFirstCase()} days</h2>
                 </div>
                 <div style={{height: "30rem"}} className="chart-container">
                     <ResponsiveLine
                         data={data}
+                        axisLeft={{format: (n: any) => {
+                            return InfectionChart.abbreviateNumber(n)
+                        }}}
                         margin={{bottom: 50, right: 50, left: 50, top: 50}}
                         enablePoints={false}
                         enableGridX={false}
                         enableGridY={true}
-                        gridYValues={[1, 2, 3, 4, 5]}
                         isInteractive
                         enableSlices={'x'}
                         sliceTooltip={(props: SliceTooltipProps) => {
@@ -85,5 +87,35 @@ export default class InfectionChart extends Component<InfectionChartProps, Infec
         const copy = new Date(Number(date));
         copy.setDate(date.getDate() + days);
         return copy
+    }
+
+    static abbreviateNumber(number: any): number {
+        // 2 decimal places => 100, 3 => 1000, etc
+        let decPlaces = Math.pow(10,1);
+
+        // Enumerate number abbreviations
+        let abbrev = [ "k", "m", "b", "t" ];
+
+        // Go through the array backwards, so we do the largest first
+        for (let i=abbrev.length-1; i>=0; i--) {
+
+            // Convert array index to "1000", "1000000", etc
+            let size = Math.pow(10,(i+1)*3);
+
+            // If the number is bigger or equal do the abbreviation
+            if(size <= number) {
+                // Here, we multiply by decPlaces, round, and then divide by decPlaces.
+                // This gives us nice rounding to a particular decimal place.
+                number = Math.round(number*decPlaces/size)/decPlaces;
+
+                // Add the letter for the abbreviation
+                number += abbrev[i];
+
+                // We are done... stop
+                break;
+            }
+        }
+
+        return number;
     }
 }
