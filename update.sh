1826517e1d7f99aad2238817711dcc699155526d
Ryan McCrystal <rrmccrystal@gmail.com>
2020-10-19 18:44:16 +0000

    removed htpts

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@ git pull --force
 docker build -t covid .
 docker stop covid
 docker rm covid
-docker run --name covid -e VIRTUAL_HOST=covid2019.tech -e VIRTUAL_PORT=8080 -e LETSENCRYPT_HOST=covid2019.tech -e LETSENCRYPT_EMAIL=rrmccrystal@gmail.com --network=draven_default --restart unless-stopped -d covid:latest
+docker run --name covid -e VIRTUAL_HOST=covid2019.tech -e VIRTUAL_PORT=8080 --network=draven_default --restart unless-stopped -d covid:latest
